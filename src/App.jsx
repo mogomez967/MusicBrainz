32c2a5d2f4663c1ce1bcf0a0fc4ecf6d0ffda63e
mogomez967 <83328456+mogomez967@users.noreply.github.com>
2024-04-13 01:32:37 +0000

    Added fetch data for Angelmaker

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,13 @@ export default function App() {
 }
 
 const root = "https://musicbrainz.org/ws/2/";
+const Angelmaker = "c5f8c1d7-9823-4b1a-bdfc-2995f847600b"
 
 // Get artist, country, title, date, and Status
-function get_music() {
-  fetch(root + "artist/1?inc=name,sort-name,url-rels,area-rels");
-  return result;
+async function get_music() {
+  const response = await fetch(root + Angelmaker + "?inc=aliases");
+  const data = await response.json();
+  console.log(data);
 }
+
+get_music();
