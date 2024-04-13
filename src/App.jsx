c0332f8de93de5c93d8498b9b2a9f879977ed16b
mogomez967 <83328456+mogomez967@users.noreply.github.com>
2024-04-13 04:21:47 +0000

    I think I got the albums?

--- src/App.jsx
+++ src/App.jsx
@@ -19,3 +19,12 @@ async function get_music() {
 }
 
 get_music();
+
+// List all albums using the release API.
+async function get_albums() {
+  const response = await fetch("https://musicbrainz.org/ws/2/artist/c5f8c1d7-9823-4b1a-bdfc-2995f847600b/releases/?fmt=json&inc=artist-credits+recordings");
+  const data = await response.json();
+  console.log(data);
+}
+
+get_albums();
