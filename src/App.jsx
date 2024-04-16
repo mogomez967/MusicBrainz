ae4fcb533ebc839cfca57402355ad66889610a13
mogomez967 <83328456+mogomez967@users.noreply.github.com>
2024-04-16 05:00:40 +0000

    Getting closer to pulling all API info

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,30 @@ export default function App() {
   )
 }
 
-const root = "https://musicbrainz.org/ws/2/";
-const Angelmaker = "artist/c5f8c1d7-9823-4b1a-bdfc-2995f847600b"
+const API = "https://musicbrainz.org/ws/2/";
+const Angelmaker = "artist/c5f8c1d7-9823-4b1a-bdfc-2995f847600b";
 
-// Get artist, country, title, date, and Status
-async function get_music() {
-  const response = await fetch("https://musicbrainz.org/ws/2/artist/c5f8c1d7-9823-4b1a-bdfc-2995f847600b?fmt=json");
-  const data = await response.json();
-  console.log(data);
-}
+// Get artist, country
+// async function get_music() {
+//   const response = await fetch(API+Angelmaker+"?fmt=json");
+//   const data = await response.json();
 
-get_music();
+//   console.log(data);
+//   console.log(data.name, data.area.name);
+// }
 
-// List all albums using the release API.
+// TODO: https://musicbrainz.org/artist/c5f8c1d7-9823-4b1a-bdfc-2995f847600b/details
+// I am missing the "release country" parameter and I suspect I am missing a url parameter.
+
+// List all albums using the release API. Get title, date, and Status
 async function get_albums() {
-  const response = await fetch("https://musicbrainz.org/ws/2/artist/c5f8c1d7-9823-4b1a-bdfc-2995f847600b/releases/?fmt=json&inc=artist-credits+recordings");
+  const response = await fetch(API+Angelmaker+"/releases/?fmt=json&inc=release-groups");
   const data = await response.json();
-  console.log(data);
+  
+  for (let i = 0; i < data['release-groups'].length; i++) {
+    console.log(data['release-groups'][i].title, data['release-groups'][i]['first-release-date'], data['release-groups'][i]['primary-type']);
+  }
 }
 
+// get_music();
 get_albums();
