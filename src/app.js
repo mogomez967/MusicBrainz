9676eba3620dfdc9bc4c9eccfab19f643f7ea376
mogomez967 <83328456+mogomez967@users.noreply.github.com>
2024-04-22 21:21:06 +0000

    Created the script version that runs with Node.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,12 @@
-import './App.css'
-
-export default function App() {
-  return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit
-    </main>
-  )
-}
-
+import fetch from 'node-fetch';
+import fs from 'fs';
 const API = "https://musicbrainz.org/ws/2/";
 const artist_input = "Angelmaker";
 
 async function get_artist() {
   const response = await fetch(API+'artist/?query=artist:' + artist_input + '&fmt=json');
   const data = await response.json();
-
+  
   if (data.artists && data.artists.length > 0) {
     console.log(data.artists[0].name);
     return data.artists[0].name;
@@ -32,22 +24,21 @@ async function get_albums(artist) {
   return csv;
 }
 
-// make_csv cannot run on browser side as is. This only works as a .js script
-// async function make_csv(data) {
-//   const csvContent = data.join('\n');
-//   const header = 'Artist,Country,Title,Date,Status\n';
-//   fs.writeFile('output.csv', header + csvContent, (err) => {
-//     if (err) throw err;
-//     console.log('CSV file created successfully');
-//   });
-// }
+async function make_csv(data) {
+  const csvContent = data.join('\n');
+  const header = 'Artist,Country,Title,Date,Status\n';
+  fs.writeFile('output.csv', header + csvContent, (err) => {
+    if (err) throw err;
+    console.log('CSV file created successfully');
+  });
+}
 
 async function main() {
   const artist = await get_artist();
   const album_data = await get_albums(artist);
   console.log(album_data);
 
-  // make_csv(album_data);
+  make_csv(album_data);
 }
 
 main();
