ac2141b8094b8d5ef8c421db906fe530d0a6ce69
mogomez967 <83328456+mogomez967@users.noreply.github.com>
2024-04-24 00:33:59 +0000

    Made it less pythonic

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ import fs from 'fs';
 const API = "https://musicbrainz.org/ws/2/";
 
 async function get_artist(artist_name) {
-  const response = await fetch(API + 'artist/?query=artist:' + artist_name + '&fmt=json');
+  const response = await fetch(`${API}artist/?query=artist:${artist_name}&fmt=json`);
   const data = await response.json();
-  
-  if (data.artists && data.artists.length > 0) {
+
+  if (data?.artists?.length > 0) {
     console.log(data.artists[0].name);
     return data.artists[0].name;
   } else {
@@ -17,8 +17,10 @@ async function get_artist(artist_name) {
 
 // List all albums using the release API. Get title, date, and Status
 async function get_albums(artist) {
-  const response = await fetch(API + 'release/?query=artist:' + artist + '&fmt=json');
+  const response = await fetch(`${API}release/?query=artist:${artist}&fmt=json`);
   const data = await response.json();
+
+  // use duoble quotes. What if album is was "The, end"?
   const csv = data.releases.map(release => [artist, release.country, release.title, release.date, release.status].join(','));
   return csv;
 }
