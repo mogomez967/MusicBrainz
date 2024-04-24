0c0fd67784a2eeb37377f865835c7944407380d1
mogomez967 <83328456+mogomez967@users.noreply.github.com>
2024-04-24 00:37:12 +0000

    Updated syntax of react file

--- src/App.jsx
+++ src/App.jsx
@@ -25,10 +25,10 @@ const API = "https://musicbrainz.org/ws/2/";
 const artist_input = "Angelmaker";
 
 async function get_artist() {
-  const response = await fetch(API+'artist/?query=artist:' + artist_input + '&fmt=json');
+  const response = await fetch(`${API}artist/?query=artist:${artist_name}&fmt=json`);
   const data = await response.json();
 
-  if (data.artists && data.artists.length > 0) {
+  if (data?.artists?.length > 0) {
     console.log(data.artists[0].name);
     return data.artists[0].name;
   } else {
@@ -39,7 +39,7 @@ async function get_artist() {
 
 // List all albums using the release API. Get title, date, and Status
 async function get_albums(artist) {
-  const response = await fetch(API + 'release/?query=artist:' + artist + '&fmt=json');
+  const response = await fetch(`${API}release/?query=artist:${artist}&fmt=json`);
   const data = await response.json();
   const csv = data.releases.map(release => [artist, release.country, release.title, release.date, release.status].join(','));
   return csv;
